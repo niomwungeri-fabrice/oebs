229ee46351ab652da797d07cca1d2adcd291813b
Fabrice Niyomwungeri <niomwungeri.fabrice@gmail.com>
2024-10-15 16:37:09 -0600

    add cors mappping for mobile app testing

--- src/main/java/io/lynx/oebs/configs/WebConfig.java
+++ src/main/java/io/lynx/oebs/configs/WebConfig.java
@@ -2,6 +2,7 @@ package io.lynx.oebs.configs;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/swagger-ui/index.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8000") // Allow Flutter web app
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
